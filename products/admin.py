26ac3156ab0b9f400c18b41cac0d93e52f5c3648
Arjun <aj110arjun@gmail.com>
2025-08-17 15:39:39 +0545

    products updated

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Product, ProductVariant, Category
+from .models import Product, ProductVariant, Category, ProductImage
 
 
 admin.site.register(Product)
 admin.site.register(ProductVariant)
 admin.site.register(Category)
+admin.site.register(ProductImage)
+
