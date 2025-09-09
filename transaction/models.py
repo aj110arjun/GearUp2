44898c4cac7d11ef451525269fc482a1a72baf19
Arjun <aj110arjun@gmail.com>
2025-09-10 04:07:05 +0530

    updates on week12 has updated

--- transaction/models.py
+++ transaction/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from decimal import Decimal
+import random
 
 class Transaction(models.Model):
     TRANSACTION_TYPE_CHOICES = [
@@ -22,6 +23,7 @@ class Transaction(models.Model):
     description = models.TextField(blank=True)
     order = models.ForeignKey('orders.Order', on_delete=models.SET_NULL, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    transaction_id = models.CharField(max_length=14, unique=True, blank=True, null=True)
 
     class Meta:
         ordering = ['-timestamp']
@@ -30,3 +32,14 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.transaction_type} - ₹{self.amount} on {self.timestamp.strftime('%Y-%m-%d %H:%M')}"
+
+    def save(self, *args, **kwargs):
+        if not self.transaction_id:
+            self.transaction_id = self.generate_unique_transaction_id()
+        super().save(*args, **kwargs)
+
+    def generate_unique_transaction_id(self):
+        while True:
+            transaction_id = str(random.randint(10**13, 10**14 - 1))
+            if not Transaction.objects.filter(transaction_id=transaction_id).exists():
+                return transaction_id
--- transaction/urls.py
+++ transaction/urls.py
@@ -1,7 +1,9 @@
 # transactions/urls.py
 from django.urls import path
-from .views import admin_transaction_list
+from . import views
 
 urlpatterns = [
-    path('custom/admin/transactions/', admin_transaction_list, name='admin_transaction_list'),
+    path('custom/admin/transactions/', views.admin_transaction_list, name='admin_transaction_list'),
+    path('custom/admin/transactions/detail/<str:transaction_id>', views.admin_transaction_detail, name='admin_transaction_detail'),
+    
 ]
