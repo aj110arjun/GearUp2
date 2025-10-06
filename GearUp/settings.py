f66e530124f47001140271606064a7a2f7d9da75
Arjun AJ <aj110arjun@gmail.com>
2025-10-06 10:06:30 +0530

    name validation on account is updated

--- GearUp/settings.py
+++ GearUp/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # DEBUG = config("DEBUG", default=False, cast=bool)
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
