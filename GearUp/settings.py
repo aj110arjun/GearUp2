9674d8db83262aaf73ccb7aaf8594e6cc7eb037b
Arjun AJ <aj110arjun@gmail.com>
2025-10-08 14:16:20 +0530

    user listing for admin updated

--- GearUp/settings.py
+++ GearUp/settings.py
@@ -120,7 +120,6 @@ DATABASES = {
 #     }
 # }
 
-#hello
 
 EMAIL_BACKEND = config("EMAIL_BACKEND")
 EMAIL_HOST = config("EMAIL_HOST")
@@ -171,7 +170,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
