d21324f9e81c73fa071565b5fa8df472643bbfaf
Arjun AJ <aj110arjun@gmail.com>
2025-10-11 14:13:34 +0530

    wishlist on product listing bug fixed

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("list/", views.wishlist_view, name="wishlist_view"),
     path("add/<int:pk>/", views.add_to_wishlist, name="add_to_wishlist"),
     path("remove/<int:pk>/", views.remove_from_wishlist, name="remove_from_wishlist"),
+    path('wishlist/toggle/<int:product_id>/', views.toggle_wishlist, name='toggle_wishlist'),
+
 ]
